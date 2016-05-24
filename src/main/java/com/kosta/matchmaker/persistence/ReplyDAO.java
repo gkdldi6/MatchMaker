0f1d441f45d51bf1d37af5ee9b87c15322773f13
kosta <kosta@192.168.0.184>
2016-05-24 21:45:34 +0900

    5/24 댓글코딩 (~376p)

---
+++ src/main/java/com/kosta/matchmaker/persistence/ReplyDAO.java
@@ -0,0 +1,17 @@
+package com.kosta.matchmaker.persistence;
+
+import java.util.List;
+
+import com.kosta.matchmaker.domain.ReplyVO;
+
+public interface ReplyDAO {
+
+	public void create(ReplyVO reply) throws Exception;
+
+	public List<ReplyVO> list(Integer bno) throws Exception;
+
+	public void update(ReplyVO reply) throws Exception;
+
+	public void delete(Integer rno) throws Exception;
+
+}
---
+++ src/main/java/com/kosta/matchmaker/service/ReplyService.java
@@ -0,0 +1,18 @@
+package com.kosta.matchmaker.service;
+
+import java.util.List;
+
+import com.kosta.matchmaker.domain.ReplyVO;
+
+public interface ReplyService {
+
+	public void addReply(ReplyVO reply) throws Exception;
+	
+	public List<ReplyVO> listReply(Integer bno) throws Exception;
+	
+	public void modifyReply(ReplyVO reply) throws Exception;
+	
+	public void deleteReply(Integer rno) throws Exception;
+	
+	
+}
