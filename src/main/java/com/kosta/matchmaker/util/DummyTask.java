58369660f933c9d53c6c6f96b1213c72839902cb
Jin <gkdldi5@naver.com>
2016-07-08 02:58:35 +0900

    콰츠 스케줄링 성공

---
+++ src/main/java/com/kosta/matchmaker/util/DummyTask.java
@@ -0,0 +1,12 @@
+package com.kosta.matchmaker.util;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class DummyTask {
+	
+	@Scheduled(cron="0/1 * * * * ?")
+    public void print() {
+        System.out.println("Spring 4.0 + Quartz 2.2 ");        
+    }
+ 
+}
